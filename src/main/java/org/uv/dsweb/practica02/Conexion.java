036c720baad6ab4f1d60558ea41b18992d96ade6
lalomg0512 <jlrj01234@gmail.com>
2024-12-03 13:58:50 -0600

    Practica02

---
+++ src/main/java/org/uv/dsweb/practica02/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.dsweb.practica02;
+
+/**
+ *
+ * @author Sebastian
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/ejemplo";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión establecida correctamente.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        if (connection == null) {
+            System.out.println("La conexión es nula.");
+        }
+        return connection;
+    }
+
+}
